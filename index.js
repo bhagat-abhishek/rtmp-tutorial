0d2497adefc4304c72449abe40b27dcdb0f67acf
bhagat-abhishek <inst.abhishek@gmail.com>
2024-03-23 22:57:46 +0530

    sending the stream as binary to server from frontend via socket.io

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import http from "http";
 import express from "express";
 import path from "path";
+import { Server as SocketIO } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new SocketIO(server);
 
 app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("Socket connected", socket.id);
+
+  //   Listening for event binarystream from frontend
+  socket.on("binarystream", (stream) => {
+    console.log("Binary Stream:", stream);
+  });
+});
 
 server.listen(3000, () => console.log("Server Listining at PORT: 3000"));
--- public/script.js
+++ public/script.js
@@ -1,10 +1,35 @@
 const userVideo = document.getElementById("user-video");
+const startButton = document.getElementById("start-button");
+
+const state = { media: null };
+
+const socket = io();
+
+// Listining to start button click
+startButton.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 250000,
+    framerate: 25,
+  });
+
+  mediaRecorder.ondataavailable = (ev) => {
+    // Sending the binary video data to server
+    socket.emit("binarystream", ev.data);
+
+    console.log("Binary data available:", ev.data);
+  };
+
+  mediaRecorder.start(25);
+});
 
 window.addEventListener("load", async (e) => {
   const media = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+
+  state.media = media;
 
   //   Setting user video
   userVideo.srcObject = media;
