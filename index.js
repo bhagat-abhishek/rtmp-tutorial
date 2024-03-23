291358148eb02d809e54e32b4bb9415a86179342
bhagat-abhishek <inst.abhishek@gmail.com>
2024-03-23 23:59:15 +0530

    final changes

--- index.js
+++ index.js
@@ -1,46 +1,58 @@
-import http from "http";
-import express from "express";
-import path from "path";
-import { spawn } from "child_process";
-import { Server as SocketIO } from "socket.io";
+import http from 'http'
+import path from 'path'
+import { spawn } from 'child_process'
+import express from 'express'
+import { Server as SocketIO } from 'socket.io'
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketIO(server);
+const io = new SocketIO(server)
 
-// options
 const options = [
-  '-i',
-  '-',
-  '-c:v', 'libx264',
-  '-preset', 'ultrafast',
-  '-tune', 'zerolatency',
-  '-r', `${25}`,
-  '-g', `${25 * 2}`,
-  '-keyint_min', 25,
-  '-crf', '25',
-  '-pix_fmt', 'yuv420p',
-  '-sc_threshold', '0',
-  '-profile:v', 'main',
-  '-level', '3.1',
-  '-c:a', 'aac',
-  '-b:a', '128k',
-  '-ar', 128000 / 4,
-  '-f', 'flv',
-  `rtmp://a.rtmp.youtube.com/live2/`
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', `${25}`,
+    '-g', `${25 * 2}`,
+    '-keyint_min', 25,
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', 128000 / 4,
+    '-f', 'flv',
+    `rtmp://a.rtmp.youtube.com/live2/56c-syd-43zp-7984-9as`,
 ];
 
-const ffmpegProcess = spawn('ffmpeg', options)
+const ffmpegProcess = spawn('ffmpeg', options);
 
-app.use(express.static(path.resolve("./public")));
-
-io.on("connection", (socket) => {
-  console.log("Socket connected", socket.id);
-
-  //   Listening for event binarystream from frontend
-  socket.on("binarystream", (stream) => {
-    console.log("Binary Stream:", stream);
-  });
+ffmpegProcess.stdout.on('data', (data) => {
+    console.log(`ffmpeg stdout: ${data}`);
 });
 
-server.listen(3000, () => console.log("Server Listining at PORT: 3000"));
+ffmpegProcess.stderr.on('data', (data) => {
+    console.error(`ffmpeg stderr: ${data}`);
+});
+
+ffmpegProcess.on('close', (code) => {
+    console.log(`ffmpeg process exited with code ${code}`);
+});
+
+app.use(express.static(path.resolve('./public')))
+
+io.on('connection', socket => {
+    console.log('Socket Connected', socket.id);
+    socket.on('binarystream', stream => {
+        console.log('Binary Stream Incommming...')
+        ffmpegProcess.stdin.write(stream, (err) => {
+            console.log('Err', err)
+        })
+    })
+})
+
+server.listen(3000, () => console.log(`HTTP Server is runnning on PORT 3000`))
--- public/script.js
+++ public/script.js
@@ -1,36 +1,29 @@
 const userVideo = document.getElementById("user-video");
 const startButton = document.getElementById("start-button");
 
-const state = { media: null };
+const state = { media: null }
+const socket = io()
 
-const socket = io();
+startButton.addEventListener('click', () => {
+    const mediaRecorder = new MediaRecorder(state.media, {
+        audioBitsPerSecond: 128000,
+        videoBitsPerSecond: 2500000,
+        framerate: 25
+    })
 
-// Listining to start button click
-startButton.addEventListener("click", () => {
-  const mediaRecorder = new MediaRecorder(state.media, {
-    audioBitsPerSecond: 128000,
-    videoBitsPerSecond: 250000,
-    framerate: 25,
-  });
+    mediaRecorder.ondataavailable = ev => {
+        console.log('Binary Stream Available', ev.data)
+        socket.emit('binarystream', ev.data)
+    }
 
-  mediaRecorder.ondataavailable = (ev) => {
-    // Sending the binary video data to server
-    socket.emit("binarystream", ev.data);
+    mediaRecorder.start(25)
 
-    console.log("Binary data available:", ev.data);
-  };
+})
 
-  mediaRecorder.start(25);
-});
-
-window.addEventListener("load", async (e) => {
-  const media = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
-
-  state.media = media;
-
-  //   Setting user video
-  userVideo.srcObject = media;
-});
+window.addEventListener('load', async e => {
+    const media = await navigator
+        .mediaDevices
+        .getUserMedia({ audio: true, video: true })
+    state.media = media
+    userVideo.srcObject = media
+})
